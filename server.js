89d71894c77c531ef430f5ed60351229e7e9b276
Leonardo Huelvas Borja <78055177+LeonardoHuelvas@users.noreply.github.com>
2023-09-01 09:19:53 -0500

    Cors  server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'https://leonardo-portafolio-web.netlify.app/'
+    origin: 'https://leonardo-portafolio-web.netlify.app/',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
